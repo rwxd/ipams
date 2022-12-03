84c8a3296f52f2ad73c1936b938edf1f8361ea0c
rwxd <rwxd@pm.me>
2022-12-04 00:19:36 +0100

    fix(setup): added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from os import path
+
+with open("./requirements.txt") as f:
+    required = f.read().splitlines()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ipams',
+    version='1.0.0',
+    description='Tool to query multiple IPAMs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='rwxd',
+    author_email='rwxd@pm.me',
+    url="https://github.com/rwxd/ipams",
+    license='MIT',
+    packages=['ipams'],
+    install_requires=required,
+    entry_points={"console_scripts": ["ipams = ipams.__main__:main"]},
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
